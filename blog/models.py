376be49502db9aaf6d709958c2eaee9324326486
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-05-29 10:36:32 +0000

    Challenge: Implement the comments model 2

--- blog/models.py
+++ blog/models.py
@@ -19,19 +19,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = 
-    author = 
-    body =
-    approved =
-    created_on = 
-
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
