f906a446b614f5ab4fee70c3986420dffa790f3a
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-05-30 17:53:11 +0000

    Templates > The base template and homepage, unsucessful loading page

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from .models import Post
 class PostList(generic.ListView):
     # Remove model = Post We can remove the model = Post as it is made redundant by the queryset 
     queryset = Post.objects.filter(status=1)    # before this row looked like this: queryset = Post.objects.all() but now we put a filter on it so that it not publish drafts!
-    template_name = "post_list.html"
+    # Replaced by instruction: template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 # def my_blog(request):
 # Deleted according to instructions:   return HttpResponse("Hello, Blog!")
